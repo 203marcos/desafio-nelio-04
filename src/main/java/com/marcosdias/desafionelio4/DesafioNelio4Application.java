effb2dc52a216eaa8566579c8b448d9fb24d9033
Marcos Dias <marcosaurelioce@hotmail.com>
2026-03-25 13:12:58 -0300

    chore: initial spring boot project

---
+++ src/main/java/com/marcosdias/desafionelio4/DesafioNelio4Application.java
@@ -0,0 +1,13 @@
+package com.marcosdias.desafionelio4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNelio4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioNelio4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcosdias/desafionelio4/DesafioNelio4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosdias.desafionelio4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNelio4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
